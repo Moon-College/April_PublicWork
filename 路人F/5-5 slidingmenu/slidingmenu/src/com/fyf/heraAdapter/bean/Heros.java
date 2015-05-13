dd451e5784f99564b8c16d65b5c4595510af001e
fyf0815 <fyouf0815@163.com>
2015-05-13 11:32:05 +0800

    有问题
    listview点击事件没用啊

---
+++ 路人F/5-5 slidingmenu/slidingmenu/src/com/fyf/heraAdapter/bean/Heros.java
@@ -0,0 +1,27 @@
+package com.fyf.heraAdapter.bean;
+
+import android.R.integer;
+
+public class Heros {
+	private String nameString;
+	private String numberString;
+	private int    faceImg;
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	public String getNumberString() {
+		return numberString;
+	}
+	public void setNumberString(String numberString) {
+		this.numberString = numberString;
+	}
+	public int getFaceImg() {
+		return faceImg;
+	}
+	public void setFaceImg(int faceImg) {
+		this.faceImg = faceImg;
+	}
+}
