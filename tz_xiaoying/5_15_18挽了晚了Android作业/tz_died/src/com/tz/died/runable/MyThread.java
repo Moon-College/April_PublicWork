8b54e9dffe7f875a82a9cb78abc58d3f59163e25
tz_小颖 <752790234@qq.com>
2015-05-22 15:49:53 +0800

    Revert "游戏一个都不能死的第二次提交"
    This reverts commit f80a036980fd6d13f468fef275cd65d7918d773c.

--- tz_xiaoying/5_15_18挽了晚了Android作业/tz_died/src/com/tz/died/runable/MyThread.java
+++ tz_xiaoying/5_15_18挽了晚了Android作业/tz_died/src/com/tz/died/runable/MyThread.java
@@ -1,7 +1,5 @@
 package com.tz.died.runable;
 
-import java.util.Random;
-
 import com.tz.died.R;
 import com.tz.rale.Role;
 
@@ -24,24 +22,7 @@ public class MyThread extends Thread {
 	public boolean isStart;
 	private int gameType;
 	Paint paint;
-	public Role role[];
-	private Rect rect[];
-	private int gameSpan;
-	private int left;
-	public int gameStatu = 0;
-	public static final int RUNNING = 0;
-	public static final int STANDOFF = 1;
-	public static final int GAMEOVER = 2;
-	private long overTime;
-   
-	public int getGameStatu() {
-		return gameStatu;
-	}
-
-	public void setGameStatu(int gameStatu) {
-		this.gameStatu = gameStatu;
-	}
-
+	private Role role;
 	public boolean isStart() {
 		return isStart;
 	}
@@ -54,10 +35,10 @@ public class MyThread extends Thread {
 			int gameType) {
 		this.holder = holder;
 		this.context = context;
-		this.gameType = gameType;
+		this.gameType=gameType;
 		this.w = w;
 		this.h = h;
-		paint = new Paint();
+		paint=new Paint();
 		isStart = true;
 		// 初始化数组
 		paths = new int[] { R.drawable.role1_00, R.drawable.role1_01,
@@ -70,170 +51,49 @@ public class MyThread extends Thread {
 					context.getResources(), paths[i]);
 			bitmaps[i] = bitmap;
 		}
-		role = new Role[gameType];
-		rect = new Rect[gameType];
-		gameSpan = 4 * h / (5 * gameType);
-		initSpirit();
-	}
+		role = new Role(bitmaps);
+		role.setX(200);
+		role.setY(200);
 
-	public void initSpirit() {
-		// TODO Auto-generated method stub
-		for (int i = 0; i < gameType; i++) {
-
-			left = 3 * w / 2;
-			int lineY = h / 10 + (i + 1) * gameSpan;
-			int roleY = lineY - bitmaps[0].getHeight();
-			int randomX = (int) (bitmaps[0].getWidth()
-					* (Math.random() * 5 + 1) / 4);
-			int randomY = (int) (bitmaps[0].getHeight()
-					* (Math.random() * 5 + 1) / 4);
-
-			// 创建障碍物
-			Rect rect = new Rect();
-			rect.left = left;
-			rect.right = rect.left + randomY;
-
-			rect.bottom = lineY;
-			rect.top = rect.bottom - randomX;
-			this.rect[i] = rect;
-			// 创建人物
-			Role role = new Role(bitmaps);
-			role.setX(w / 10 + 20);
-			role.setY(roleY);
-			this.role[i] = role;
-
-		}
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		super.run();
-		// 执行画画任务
-		while (isStart) {
-			// 开始获取画布来绘制
-			Canvas canvas = null;
-			try {
-				canvas = holder.lockCanvas();// 将获取的画布绑定给画板
-				if (canvas != null) {
-					canvas.drawColor(Color.WHITE);// 绘制白色的背景
-
-					// 最后确认画布OK了可以画画了
-					// 说明游戏根据阶段不同所要绘制的东西不同
-					switch (gameStatu) {
-					case RUNNING:
-						// 正常运行
-						drawRunningView(canvas);
-						break;
-					case STANDOFF:
-						// 僵持状态
-						drawStandOffView(canvas);
-						break;
-					case GAMEOVER:
-						// 游戏结束状态
-						drawGameOverView(canvas);
-						break;
-					default:
-						break;
+		//执行画画任务
+				while(isStart){
+					//开始获取画布来绘制
+					Canvas canvas = null;
+					try{
+						canvas = holder.lockCanvas();//将获取的画布绑定给画板
+						if(canvas!=null){
+							//最后确认画布OK了可以画画了
+							canvas.drawColor(Color.WHITE);//绘制白色的背景
+							drawRectLine(canvas);
+						}
+					}catch(Exception e){
+						
+					}finally{
+						if(canvas!=null){
+							holder.unlockCanvasAndPost(canvas);
+						}
 					}
-				}
-			} catch (Exception e) {
-
-			} finally {
-				if (canvas != null) {
-					holder.unlockCanvasAndPost(canvas);
-				}
-			}
-
+			
 		}
 	}
 
-	private void drawRunningView(Canvas canvas) {
-
-		for (int i = 0; i < gameType; i++) {
-			// 第一个i的线的距离
-			int lineY = h / 10 + (i + 1) * gameSpan;
-			int roleY = lineY - bitmaps[0].getHeight();
-			// 障碍物从右往左移
-			rect[i].left = rect[i].left - h / 150;
-			rect[i].right = rect[i].right - h / 150;
-			if (rect[i].left < 0) {
-				int randomX = (int) (bitmaps[0].getWidth()
-						* (Math.random() * 5 + 1) / 4);
-				int randomY = (int) (bitmaps[0].getHeight()
-						* (Math.random() * 5 + 1) / 4);
-				// 创建障碍物
-				rect[i].left = left;
-				rect[i].right = rect[i].left + randomY;
-				rect[i].top = rect[i].bottom - randomX;
-
-			}
-			// 向下移的方法
-			role[i].setSpeedY(role[i].getSpeedY() + h / 800f);
-			role[i].setY(role[i].getY() + role[i].getSpeedY());
-			// 判断不能超过线的距离
-			if (role[i].getY() + role[i].getHeight() >= lineY) {
-				role[i].setJump(false);
-				role[i].setY(roleY);
-			}
-			if (rect[i].intersect(role[i].getRolerect())) {
-				gameStatu = STANDOFF;
-				overTime = System.currentTimeMillis();
-			}
-
-			role[i].drawSelf(canvas);
-			canvas.drawLine(0, lineY, w, lineY, paint);
-			// 绘制一个障碍物
-			canvas.drawRect(rect[i], paint);
-		}
-
-	}
-
-	/**
-	 * 游戏僵持状态
-	 * 
-	 * @param canvas
-	 */
-	private void drawStandOffView(Canvas canvas) {
-		if (System.currentTimeMillis() - overTime >= 1000) {
-              gameStatu=GAMEOVER;
-		} else {
-			for (int i = 0; i < gameType; i++) {
-				// 第一个i的线的距离
-				int lineY = h / 10 + (i + 1) * gameSpan;
-				// 画线
-				canvas.drawLine(0, lineY, w, lineY, paint);
-				// 画一个人
-				this.role[i].drawSelf(canvas);
-				// 绘制一个障碍物
-				canvas.drawRect(this.rect[i], paint);
-			}
-		}
-	}
-
-	/**
-	 * 游戏结束
-	 * 
-	 * @param canvas
-	 */
-	private void drawGameOverView(Canvas canvas) {
+	private void drawRectLine(Canvas canvas) {
 		// TODO Auto-generated method stub
-        canvas.drawColor(Color.RED);
-		//绘制文字
-		String [] modes = new String[]{
-				"普通","噩梦","地狱","炼狱"
-		};
-		String back="返回";
-		String reset="重来";
-		//绘制3段文本
-		drawText(canvas, modes[gameType-2], h/2,28);
-		drawText(canvas, back, h*4/6,20);
-		drawText(canvas, reset, h*5/6,20);
+		int gameSpan=4*h/(5*gameType);
+		for(int i=0;i<gameType;i++){
+			int lineY=h/10+(i+1)*gameSpan;
+			int roleY=lineY-bitmaps[0].getHeight();
+			role.setX(h/10+20);
+			role.setY(roleY);
+			role.drawSelf(canvas);
+			canvas.drawLine(0, lineY, w, lineY, paint);
+		}
 		
-	}
-	public void drawText(Canvas canvas,String text,float y,int textSize){
-		paint.setTextSize(textSize);
-		float measureText = paint.measureText(text);//文本的宽度
-		canvas.drawText(text, (w-measureText)/2, y, paint);
 	}
 }
--- tz_xiaoying/5_15_18挽了晚了Android作业/tz_died/src/com/tz/rale/Role.java
+++ tz_xiaoying/5_15_18挽了晚了Android作业/tz_died/src/com/tz/rale/Role.java
@@ -2,134 +2,93 @@ package com.tz.rale;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Rect;
 
 public class Role {
-	private Bitmap bitmap;// 当前图片
-	private Bitmap[] bms;// 完成一个动作的图片数组
-	float x, y;// 坐标
-	float speedX, speedY;// 速度
-	int width, height;// 宽高
+	private Bitmap bitmap;//当前图片
+	private Bitmap[] bms;//完成一个动作的图片数组
+	float x,y;//坐标
+	float speedX,speedY;//速度
+	int width,height;//宽高
 	private long lastTime;
 	private int index;
-    private boolean isJump;
-	public Role(Bitmap[] bms) {
+	public Role(Bitmap[] bms){
 		this.bms = bms;
 		this.bitmap = bms[0];
 		this.width = bitmap.getWidth();
 		this.height = bitmap.getHeight();
 	}
-    
-	public boolean isJump() {
-		return isJump;
-	}
-
-	public void setJump(boolean isJump) {
-		this.isJump = isJump;
-	}
-
+	
 	public Bitmap getBitmap() {
 		return bitmap;
 	}
-
 	public void setBitmap(Bitmap bitmap) {
 		this.bitmap = bitmap;
 	}
-
 	public Bitmap[] getBms() {
 		return bms;
 	}
-
 	public void setBms(Bitmap[] bms) {
 		this.bms = bms;
 	}
-
 	public float getX() {
 		return x;
 	}
-
 	public void setX(float x) {
 		this.x = x;
 	}
-
 	public float getY() {
 		return y;
 	}
-
 	public void setY(float y) {
 		this.y = y;
 	}
-
 	public float getSpeedX() {
 		return speedX;
 	}
-
 	public void setSpeedX(float speedX) {
 		this.speedX = speedX;
 	}
-
 	public float getSpeedY() {
 		return speedY;
 	}
-
 	public void setSpeedY(float speedY) {
 		this.speedY = speedY;
 	}
-
 	public int getWidth() {
 		return width;
 	}
-
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	
 	/**
 	 * 切换图片开启动画
-	 * 
 	 * @param span
 	 */
-	private void animateRole(long span) {
-		// 每隔一小段时间就切换当前图片
-		if (System.currentTimeMillis() - lastTime >= span) {
-			// 说明时间到了，该切图了
-			index++;
-			if (index == bms.length) {
+	private void animateRole(long span){
+		//每隔一小段时间就切换当前图片
+		if(System.currentTimeMillis() - lastTime>=span){
+			//说明时间到了，该切图了
+			index ++;
+			if(index ==bms.length){
 				index = 0;
 			}
 			bitmap = bms[index];
 			lastTime = System.currentTimeMillis();
 		}
 	}
-
 	/**
 	 * 将该任务绘制到画布上
 	 */
-	public void drawSelf(Canvas canvas) {
+	public void drawSelf(Canvas canvas){
 		animateRole(200);
-		// 绘制自己
-		canvas.drawBitmap(bitmap, this.getX(), this.getY(), null);
-	}
-
-	/***
-	 * 得到人物的矩形
-	 * 
-	 */
-	public Rect getRolerect() {
-		Rect rect = new Rect();
-		rect.left = (int) this.getX();
-		rect.right = (int) this.getX() + this.getWidth();
-		rect.bottom = (int) this.getY() + this.getHeight();
-		rect.top = (int) this.getY();
-		return rect;
-
+		//绘制自己
+		canvas.drawBitmap(bitmap, this.getX(),this.getY(),null);
 	}
 }
