127fb3e856daee8c5f4fe2da05b9d79e0101aea7
fyf0815 <fyouf0815@163.com>
2015-05-25 10:07:21 +0800

    一个都不能死生命版

--- 路人F/一个都不能死/nodiegame/src/com/fyf/nodie/people/Role.java
+++ 路人F/一个都不能死/nodiegame/src/com/fyf/nodie/people/Role.java
@@ -3,6 +3,7 @@ package com.fyf.nodie.people;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.Log;
 
 public class Role {
@@ -13,6 +14,7 @@ public class Role {
 	int width,heigth;
 	private long lastTime;
 	private int index;
+	private boolean isJump;
 	
 	public Role(Bitmap[] bms,float x,float y){
 		this.bms = bms;
@@ -27,8 +29,7 @@ public class Role {
 	 * @param span
 	 */
 	private void animateRole(long span){
-		Long nowTimeLong = System.currentTimeMillis();
-		if((nowTimeLong-lastTime) >=span){
+		if((System.currentTimeMillis()-lastTime) >=span){
 			//切换图片
 			index ++;
 			if(index ==bms.length){
@@ -43,50 +44,75 @@ public class Role {
 	 * @param canvas
 	 */
 	public void drawSelf(Canvas canvas){
-		animateRole(200);
+		animateRole(100);
 		canvas.drawBitmap(bitmap, this.getX(),this.getY(),null);
 	}
+	/**
+	 * 获取人物矩形
+	 * @return
+	 */
+	public Rect getRectFromRole(){
+		Rect rect = new Rect();
+		rect.left = (int) this.getX();
+		rect.right = (int) (this.getX() + this.getWidth());
+		rect.top = (int) this.getY();
+		rect.bottom = (int) (this.getY() + this.getHeigth());
+		return rect;
+	}
 	
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeigth() {
+		return heigth;
+	}
+	public void setHeigth(int heigth) {
+		this.heigth = heigth;
+	}	
 	public Bitmap getBitmap() {
 		return bitmap;
 	}
 	public void setBitmap(Bitmap bitmap) {
 		this.bitmap = bitmap;
 	}
-	
 	public Bitmap[] getBms() {
 		return bms;
 	}
 	public void setBms(Bitmap[] bmsBitmaps) {
 		this.bms = bmsBitmaps;
 	}
-	
 	public float getX() {
 		return x;
 	}
 	public void setX(float x) {
 		this.x = x;
 	}
-	
 	public float getY() {
 		return y;
 	}
 	public void setY(float y) {
 		this.y = y;
 	}
-	
 	public float getSpeedX() {
 		return speedX;
 	}
 	public void setSpeedX(float speedX) {
 		this.speedX = speedX;
-	}
-	
+	}	
 	public float getSpeedY() {
 		return speedY;
 	}
 	public void setSpeedY(float speedY) {
 		this.speedY = speedY;
 	}
-	
+	public boolean isJump() {
+		return isJump;
+	}
+	public void setJump(boolean isJump) {
+		this.isJump = isJump;
+	}	
 }
