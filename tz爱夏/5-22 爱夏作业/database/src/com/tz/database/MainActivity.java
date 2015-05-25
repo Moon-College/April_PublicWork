745532a0150247e7b765d7fafbd4631aa334e395
潭州-爱夏-深圳 <13168750594@163.com>
2015-05-25 19:16:39 +0800

    sqlite修改版

--- tz爱夏/5-22 爱夏作业/database/src/com/tz/database/MainActivity.java
+++ tz爱夏/5-22 爱夏作业/database/src/com/tz/database/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tz.database;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -7,36 +9,41 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.tz.database.bean.Teacher;
 import com.tz.database.service.MyDatabase;
 import com.tz.database.service.OperateTable;
 
 public class MainActivity extends Activity implements OnClickListener {
-	private Button database,add,delete,update,query,deleteAll;
+	private Button database, add, delete, update, query, deleteAll;
 	private MyDatabase myDatabase;
 	private OperateTable operrate;
 	private SQLiteDatabase db;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        myDatabase=new MyDatabase();
-        operrate=new OperateTable();
-        initView();
-        
-    }
-    
-    /**
-     * @author jackie
-     */
+	private Teacher teacher;
+	private List<Teacher> teachers;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		myDatabase = new MyDatabase();
+		operrate = new OperateTable();
+		initView();
+
+	}
+
+	/**
+	 * @author jackie
+	 */
 	private void initView() {
-		database=(Button) findViewById(R.id.database);
-		add=(Button) findViewById(R.id.add);
-		delete=(Button) findViewById(R.id.delete);
-		deleteAll=(Button) findViewById(R.id.deleteAll);
-		update=(Button) findViewById(R.id.update);
-		query=(Button) findViewById(R.id.query);
+		database = (Button) findViewById(R.id.database);
+		add = (Button) findViewById(R.id.add);
+		delete = (Button) findViewById(R.id.delete);
+		deleteAll = (Button) findViewById(R.id.deleteAll);
+		update = (Button) findViewById(R.id.update);
+		query = (Button) findViewById(R.id.query);
 		database.setOnClickListener(this);
 		add.setOnClickListener(this);
 		delete.setOnClickListener(this);
@@ -46,16 +53,16 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	public void onClick(View v) {
-           switch (v.getId()) {
+		switch (v.getId()) {
 		case R.id.database:
-			db=myDatabase.createOrOpenDatabase(this);
-			try{
-			myDatabase.createTable();
-			}catch(Exception e){
+			db = myDatabase.createOrOpenDatabase(this);
+			try {
+				myDatabase.createTable();
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			break;
-			
+
 		case R.id.add:
 			operrate.addData(db);
 			break;
@@ -69,11 +76,28 @@ public class MainActivity extends Activity implements OnClickListener {
 			operrate.updateDataById(db, 3);
 			break;
 		case R.id.query:
-			operrate.quaryAllData(db);
+			teachers = operrate.quaryAllData(db);
+			for (Teacher teacher : teachers) {
+				Log.i("INFO",
+						"id:" + teacher.getId() + "-name:" + teacher.getName()
+								+ "-age:" + teacher.getAge() + "-hobby:"
+								+ teacher.getHobby());
+				Toast.makeText(
+						this,
+						"id:" + teacher.getId() + "-name:" + teacher.getName()
+								+ "-age:" + teacher.getAge() + "-hobby:"
+								+ teacher.getHobby(), 1000).show();
+			}
 			break;
 
 		default:
 			break;
-		}		
+		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		db.close();
 	}
 }
--- tz爱夏/5-22 爱夏作业/database/src/com/tz/database/service/OperateTable.java
+++ tz爱夏/5-22 爱夏作业/database/src/com/tz/database/service/OperateTable.java
@@ -1,12 +1,18 @@
 package com.tz.database.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import com.tz.database.bean.Teacher;
+
 public class OperateTable {
-       
+       private Teacher teacher;
+       private List<Teacher> teachers;
 	  public void addData(SQLiteDatabase db){
 		  String sql="insert into teacher values(null,?,?,?)";
 		  db.execSQL(sql,new String[]{"Danny",String.valueOf(27),"programing"});
@@ -38,20 +44,38 @@ public class OperateTable {
 		  Log.i("UPDATE", "修改数据");
 	  }
 	  
-	  public void quaryDataById(SQLiteDatabase db,int id){
+	  public Teacher quaryDataById(SQLiteDatabase db,int id){
+		  teacher=new Teacher();
 		  String sql="select * from teacher where _id=?";
-		  db.rawQuery(sql, new String[]{"id"});
+		  Cursor cursor=db.rawQuery(sql, new String[]{String.valueOf(id)});
+		  teacher.setId(id);
+		  String  name=cursor.getString(cursor.getColumnIndex("name"));
+		  teacher.setName(name);
+		  int  age=cursor.getInt(cursor.getColumnIndex("age"));
+		  teacher.setAge(age);
+		  String  hobby=cursor.getString(cursor.getColumnIndex("hobby"));
+		  teacher.setHobby(hobby);
+		  return teacher;
 	  }
 	  
-	  public void quaryAllData(SQLiteDatabase db){
+	  public List<Teacher> quaryAllData(SQLiteDatabase db){
+		  teachers=new ArrayList<Teacher>();
 		  String sql="select * from teacher ";
 		  Cursor cursor=db.rawQuery(sql, null);
 		  while(cursor.moveToNext()){
+			  //初始化类
+			  teacher=new Teacher();
 			  //每查到一条符合条件的记录就打印
+			  int id=cursor.getInt(cursor.getColumnIndex("_id"));
+			  teacher.setId(id);
 			  String  name=cursor.getString(cursor.getColumnIndex("name"));
+			  teacher.setName(name);
 			  int  age=cursor.getInt(cursor.getColumnIndex("age"));
+			  teacher.setAge(age);
 			  String  hobby=cursor.getString(cursor.getColumnIndex("hobby"));
-			  Log.i("INFO", "name:"+name+"-age:"+age+"-hobby:"+hobby);
+			  teacher.setHobby(hobby);
+			  teachers.add(teacher);
 		  }
+		  return teachers;
 	  }
 }
